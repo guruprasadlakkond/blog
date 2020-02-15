6002df024e575aac512494658788154901156270
guruprasadlakkond <guruprasadlakkond@gmail.com>
2020-02-15 21:12:00 +0530

    fetching using lodash memoize

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 import api from '../services/api';
+import _ from 'lodash';
+
 export const fetchPosts = () => async (dispatch, getState) => {
   const response = await api.get('/posts');
 
@@ -7,11 +9,23 @@ export const fetchPosts = () => async (dispatch, getState) => {
     payload: response.data
   });
 };
-export const fetchUser = id => async (dispatch, getState) => {
+
+// export const fetchUser = id => async (dispatch, getState) => {
+//   const response = await api.get(`users/${id}`);
+
+//   dispatch({
+//     type: 'FETCH_USER',
+//     payload: response.data
+//   });
+// };
+
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await api.get(`users/${id}`);
 
   dispatch({
     type: 'FETCH_USER',
     payload: response.data
   });
-};
+});
