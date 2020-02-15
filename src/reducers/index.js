14c97d3bc763e574377cc1d703b80634a64ab5bf
guruprasadlakkond <guruprasadlakkond@gmail.com>
2020-02-15 16:56:25 +0530

    fetching user

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import posts from './posts';
+import usersReducer from './users';
 /* const posts = () => {
   return [{ id: 1, title: 'hello' }];
 }; */
-export default combineReducers({ posts });
+export default combineReducers({ posts, users: usersReducer });
