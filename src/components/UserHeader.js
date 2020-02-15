3f48931840152efa8035df1a7f2ad7d7a61060fd
guruprasadlakkond <guruprasadlakkond@gmail.com>
2020-02-15 17:06:08 +0530

    passing single user to comp

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -7,16 +7,15 @@ class Userheader extends React.Component {
     this.props.fetchUser(this.props.userId);
   }
   render() {
-    const user = this.props.users.find(user => user.id === this.props.userId);
-    if (!user) {
+    if (!this.props.user) {
       return <div>Loading......</div>;
     }
-    return <div className="header">{user.name}</div>;
+    return <div className="header">{this.props.user.name}</div>;
   }
 }
 
-const mapStateToProps = state => {
-  return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
 export default connect(mapStateToProps, { fetchUser })(Userheader);
